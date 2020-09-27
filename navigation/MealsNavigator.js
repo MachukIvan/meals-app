4051fc683647db966f06fb99f55e79d575e89831
Ivan Machuk <machuk.ivan@gmail.com>
2020-09-27 15:18:23 +0300

    Added fav btn to header, set up tab navigation

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,10 +1,15 @@
+import React from 'react';
 import { Platform } from 'react-native';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createAppContainer } from 'react-navigation';
+import { Ionicons } from '@expo/vector-icons';
+
 import CategoriesScreen from '../screens/CategoriesScreen';
 import CategoryMealsScreen from '../screens/CategoryMealsScreen';
 import MealDetailScreen from '../screens/MealDetailScreen';
-import Colors from '../constants/Colors';
+import colors from '../constants/Colors';
+import FavoritesScreen from '../screens/FavoritesScreen';
 
 const MealsNavigator = createStackNavigator(
     {
@@ -19,12 +24,46 @@ const MealsNavigator = createStackNavigator(
         defaultNavigationOptions: {
             headerStyle: {
                 backgroundColor:
-                    Platform.OS === 'android' ? Colors.primaryColor : '',
+                    Platform.OS === 'android' ? colors.primaryColor : '',
             },
             headerTintColor:
-                Platform.OS === 'android' ? 'white' : Colors.primaryColor,
+                Platform.OS === 'android' ? 'white' : colors.primaryColor,
         },
     }
 );
 
-export default createAppContainer(MealsNavigator);
+const MealsFavTabNavigator = createBottomTabNavigator(
+    {
+        Meals: {
+            screen: MealsNavigator,
+            navigationOptions: {
+                tabBarIcon: tabInfo => (
+                    <Ionicons
+                        name="ios-restaurant"
+                        size={25}
+                        color={tabInfo.tintColor}
+                    />
+                ),
+            },
+        },
+        Favorites: {
+            screen: FavoritesScreen,
+            navigationOptions: {
+                tabBarIcon: tabInfo => (
+                    <Ionicons
+                        name="ios-star"
+                        size={25}
+                        color={tabInfo.tintColor}
+                    />
+                ),
+            },
+        },
+    },
+    {
+        tabBarOptions: {
+            activeTintColor: colors.accentColor,
+        },
+    }
+);
+
+export default createAppContainer(MealsFavTabNavigator);
--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
 import { MEALS } from '../data/dummy-data';
+import HeaderButton from '../components/HeaderButton';
 
 const MealDetailScreen = ({ navigation }) => {
     const mealId = navigation.getParam('mealId');
@@ -26,6 +28,11 @@ MealDetailScreen.navigationOptions = ({ navigation }) => {
     const selectedMeal = MEALS.find(meal => meal.id === mealId);
     return {
         headerTitle: selectedMeal.title,
+        headerRight: (
+            <HeaderButtons HeaderButtonComponent={HeaderButton}>
+                <Item title="Favorite" iconName="ios-star" onPress={() => {}} />
+            </HeaderButtons>
+        ),
     };
 };
 
